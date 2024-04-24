6a47ca21d63a4fd8039b3cf39323ddbd6a99f2eb
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-04-24 23:59:46 +0300

    creating Card component

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,12 @@
 // The Card to be exported goes here
+import Image from "next/image";
+
+export const Card  = ({ image, title, description }) => {
+    return (
+        <div>
+            <Image />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div>
+    )
+}
