fceaef0273fbd3b9a77e742fdbd2354d9ebc66f0
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-04-26 17:43:55 +0300

    fixing the style for the card

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -8,11 +8,20 @@ export const StyledCardContainer = styled.div`
   margin: 1rem;
   border-radius: 15px;
   background-color: #e1e4e8;
-  border: 2px solid transparent;
-  transition: border-color 0.3s ease;
-  &:hover {
-    border-color: #006efd;
+
+  &:nth-child(1) {
+    max-width: 27rem;
   }
+
+  &:nth-child(2) {
+    max-width: 29rem;
+  }
+
+  &:nth-child(3) {
+  border: 2px solid transparent;
+  border-color: #006efd;
+  color: #006efd;
+
 `;
 
 export const StyledImageContainer = styled.div`
@@ -32,12 +41,6 @@ export const StyledTextContainer = styled(({ ...props }) => <div {...props} />)`
 export const StyledTitle = styled((props) => <SectionSubheading {...props} />)`
   margin: 0;
   font-weight: 700;
-  color: black;
-  transition: color 0.3s ease;
-
-  ${StyledCardContainer}:hover & {
-    color: #006efd;
-  }
 `;
 
 export const StyledDescription = styled((props) => (
@@ -45,5 +48,6 @@ export const StyledDescription = styled((props) => (
 ))`
   margin: 0 0;
   text-align: left;
-  width: 85%;
+  width: 90%;
+  color: black;
 `;
