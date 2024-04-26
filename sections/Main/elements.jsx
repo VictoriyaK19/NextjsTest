be26148ba18015c448411df43e38e6af56e60c9e
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-04-26 17:28:52 +0300

    adding style for mobile devices

--- sections/Main/elements.jsx
+++ sections/Main/elements.jsx
@@ -14,18 +14,33 @@ export const StyledMainContainer = styled(({ ...props }) => (
   text-align: center;
   flex-direction: column;
   margin-top: 3rem;
+
+  @media (max-width: 767px) {
+    padding: 2rem;
+  }
 `;
 
 export const StyledTitle = styled(({ ...props }) => (
   <SectionHeading {...props} />
 ))`
   margin: 0;
+  font-size: 2.5rem;
+
+  @media (max-width: 767px) {
+    font-size: 1.2rem;
+  }
 `;
 
 export const StyledDescription = styled(({ ...props }) => (
   <SectionSubheading {...props} />
 ))`
   margin-top: 0;
+  font-size: 1.3rem;
+
+  @media (max-width: 767px) {
+    font-size: 0.8rem;
+    margin: 0;
+  }
 `;
 
 export const StyledTextContainer = styled(({ ...props }) => (
@@ -33,6 +48,10 @@ export const StyledTextContainer = styled(({ ...props }) => (
 ))`
   margin: 1rem 0 2rem 0;
   flex-direction: column;
+
+  @media (max-width: 767px) {
+   margin: 0;
+  }
 `;
 
 export const StyledContainer = styled(({ ...props }) => (
@@ -52,6 +71,10 @@ export const StyledImageContainer = styled.div`
   margin: 2rem;
   max-width: 20rem;
   width: 50%;
+
+  @media (max-width: 767px) {
+    width: 55%;
+  }
 `;
 
 export const StyledCardsContainer = styled.div`
@@ -59,4 +82,9 @@ export const StyledCardsContainer = styled.div`
   margin: 2rem;
   display: flex;
   flex-direction: column;
+
+  @media (max-width: 767px) {
+    max-width: 100%;
+    margin: 1rem 0 ;
+  }
 `;
