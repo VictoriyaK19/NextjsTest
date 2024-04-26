eeea34b0ee74beea24be97ffdc21ccdd0d9170a9
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-04-26 18:15:01 +0300

    fixing style for mobile devices

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import { SectionSubheading, SectionParagraph } from "~/components";
 
 export const StyledCardContainer = styled.div`
-  max-width: 33rem;
+  max-width: 33vw;
+  width: 100%;
   align-items: center;
   display: flex;
   margin: 1rem;
@@ -10,17 +11,19 @@ export const StyledCardContainer = styled.div`
   background-color: #e1e4e8;
 
   &:nth-child(1) {
-    max-width: 27rem;
+    max-width: 85%;
   }
 
   &:nth-child(2) {
-    max-width: 29rem;
+    max-width: 90%;
   }
 
   &:nth-child(3) {
+  max-width: 95%;
   border: 2px solid transparent;
   border-color: #006efd;
   color: #006efd;
+
 
 `;
 
@@ -29,6 +32,10 @@ export const StyledImageContainer = styled.div`
   width: 70%;
   align-self: center;
   padding: 2rem;
+
+  @media (max-width: 767px) {
+    max-width: 3.5rem;
+  }
 `;
 
 export const StyledTextContainer = styled(({ ...props }) => <div {...props} />)`
@@ -36,11 +43,19 @@ export const StyledTextContainer = styled(({ ...props }) => <div {...props} />)`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+
+  @media (max-width: 767px) {
+    padding: 0.5rem 0;
+  }
 `;
 
 export const StyledTitle = styled((props) => <SectionSubheading {...props} />)`
   margin: 0;
   font-weight: 700;
+
+  @media (max-width: 767px) {
+    font-size: 1rem; 
+  }
 `;
 
 export const StyledDescription = styled((props) => (
@@ -50,4 +65,8 @@ export const StyledDescription = styled((props) => (
   text-align: left;
   width: 90%;
   color: black;
+
+  @media (max-width: 767px) {
+    font-size: 0.8rem; 
+  }
 `;
